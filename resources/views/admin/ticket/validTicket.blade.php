0dcbe1ab1e97932cbfbb2a92dffd93fea0f87c8c
Yannams <142616312+Yannams@users.noreply.github.com>
2024-06-03 17:00:32 +0200

    verification terminee

--- resources/views/admin/ticket/validTicket.blade.php
+++ resources/views/admin/ticket/validTicket.blade.php
@@ -1,1 +1,26 @@
-success
+@extends('layout.promoteur')
+    @section('content')
+    <div class="" style="height: 600px">
+        <div class="align-middle">
+            <div class="w-100 h-100 d-flex justify-content-center">
+                <div class="card border-0 w-50 h-25">
+                    <div class="card-body ">
+                        <div class="success-checkmark">
+                            <div class="check-icon">
+                            <span class="icon-line line-tip"></span>
+                            <span class="icon-line line-long"></span>
+                            <div class="icon-circle"></div>
+                            <div class="icon-fix"></div>
+                            </div>
+                        </div>
+                    
+                        <div class="w-100 d-flex justify-content-center">Ticket valide</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+   
+   
+    
+    @endsection
