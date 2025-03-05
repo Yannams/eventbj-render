46ba2e53c9dfc94c634ef11bf476b507c435ecf1
Yannams <142616312+Yannams@users.noreply.github.com>
2025-03-05 18:14:52 +0100

    control des ticket avec historiques des controles correction 3.1.2.1

--- resources/views/admin/ticket/scanTicket.blade.php
+++ resources/views/admin/ticket/scanTicket.blade.php
@@ -62,7 +62,7 @@
                 var cameraId = devices[0].id;
                 const html5QrCode = new Html5Qrcode(/* element id */ "reader");
                 html5QrCode.start(
-                  cameraId, 
+                  { facingMode: "environment" }, 
                   {
                     fps: 10,    // Optional, frame per seconds for qr code scanning
                     qrbox: { width: 250, height: 250 }  // Optional, if you want bounded box UI
