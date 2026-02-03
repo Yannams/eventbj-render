a8cdaa2ae14ae41c7fc593f7d224b25bde8366a3
yann <yannamoussou20@gmail.com>
2026-02-03 14:09:49 +0100

    laravel deploy script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,6 +2,9 @@
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
 
+echo "Installing npm packages..."
+npm install --prefix=/var/www/html
+
 echo "Caching config..."
 php artisan config:cache
 
@@ -10,6 +13,9 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force 
+
+echo "Seeding database..."
+php artisan db:seed --force
 
 echo "Publishing cloudinary provider..."
 php artisan vendor:publish --provider="CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider" --tag="cloudinary-laravel-config"
